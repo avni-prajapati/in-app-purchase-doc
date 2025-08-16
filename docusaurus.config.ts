c2556a92e0227ca0bfb4db21b6b460fad75a6b7f
Avni Prajapati <154810109+avni-prajapati@users.noreply.github.com>
2025-08-16 16:42:30 +0530

    Update docusaurus.config.ts

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -13,7 +13,7 @@ const config: Config = {
   url: 'https://avniprajapati.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/docs/in-app-purchase/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
